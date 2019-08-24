b7cf27b30f9e34be3732f6843359d27ff8ad77cd
Patrick Owens <manbearpat@gmail.com>
2019-08-24 09:36:17 -0700

    dev card info updates

--- client/src/components/DevCards/Lucious/index.js
+++ client/src/components/DevCards/Lucious/index.js
@@ -12,14 +12,14 @@ function Lucious() {
                 <img src='https://media.licdn.com/dms/image/C5603AQHsJJSTjoE-5g/profile-displayphoto-shrink_200_200/0?e=1571270400&v=beta&t=TjhMjpahllPGCHGkRwx_QgThBJkzisICGVaaFvKXwkU' className="card-img-top"></img>
                 <div className="card-body">
                     <h3>Lucious Jackson</h3>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p className="card-text">Recent graduate of the UC Davis, Full Stack Web Development program. In pursuit of attaining a front end developer career. In-depth knowledge of HTML5, CSS3, and JavaScript with the prime focus of developing unique and stunning websites that are desktop and mobile user-friendly.</p>
                     <h4>Skillset:</h4>
                     <ul>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        <li>ReactJs</li>
                     </ul>
                     <a href="http://www.linkedin.com/in/lucious-x-jackson-9a4807155"><FaLinkedin className="aboutIcon" /></a>
                 </div>
