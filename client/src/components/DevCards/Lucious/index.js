1dcd4bbaef54f8727f6cd4d6b0bb5e2ef0436d0d
LuciousX <luciousjackson49@gmail.com>
2019-08-26 10:32:28 -0700

    Update index.js
    Updated About Us

--- client/src/components/DevCards/Lucious/index.js
+++ client/src/components/DevCards/Lucious/index.js
@@ -11,15 +11,19 @@ function Lucious() {
             <div className="card dev-card">
                 <img src='https://media.licdn.com/dms/image/C5603AQHsJJSTjoE-5g/profile-displayphoto-shrink_200_200/0?e=1571270400&v=beta&t=TjhMjpahllPGCHGkRwx_QgThBJkzisICGVaaFvKXwkU' className="card-img-top"></img>
                 <div className="card-body">
-                    <h3>Lucious Jackson</h3>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <h3>Lucious Jackson</h3> 
+                    <p className="card-text">I am 5 years experienced in study and use of HTML5, CSS3, and Vanilla JavaScript. 
+                    I am also experienced in the use of the Adobe Creative Cloud Suite (PhotoShop, Adobe Illustrator, InDesign).
+                    I constantly look for ways to self-improve and level-up so, I enrolled in, completed and am a recent Graduate of the University of California Davis Full-Stack Flex Accellerator where I have received Professional training on both Frontend/Client-Side and Backend/Server-Side Technologies. I have taken an interest in Database Administration field and wish to focus on that area of the Industry as I have developed a love for working with MySQL, PostgreSQL and MongoDB.
+                    I look forward to applying my new skillset to the Employer that wishes to introduce and add a new face to their workforce that will be on-time and motivated to improve every day even after being hired!</p>
                     <h4>Skillset:</h4>
                     <ul>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
+                        <li>ReactJS</li>
+                        <li>NodeJS</li>
+                        <li>MongoDB</li>
+                        <li>MySQL/PostgreSQL</li>
+                        <li>PassportJS</li>
+                        <li>Adobe Creative Cloud Suite</li>
                     </ul>
                     <a href="http://www.linkedin.com/in/lucious-x-jackson-9a4807155"><FaLinkedin className="aboutIcon" /></a>
                 </div>
