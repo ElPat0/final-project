9605cbeedd645b9104e83e51d5c451e03ab55033
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-08-27 23:17:00 -0700

    updated card Meena in about us page

--- client/src/components/DevCards/Meena/index.js
+++ client/src/components/DevCards/Meena/index.js
@@ -13,16 +13,15 @@ function Meena() {
                 <img src="https://media.licdn.com/dms/image/C5603AQG51vY3J2NrMg/profile-displayphoto-shrink_200_200/0?e=1571270400&v=beta&t=5J70va6sfQZhCKHSqaQw_--pnbKOT8gDhnE4rpPkG-c" className="card-img-top"></img>
                 <div className="card-body">
                     <h3>Meena</h3>
-                    <p className="card-text">5+ years of experience in Software Quality Assurance - Test Automation and Manual Testing. Good communication and presentation skills, detail oriented, self starter, quick learner, and team player
-                    Working on test automation of web applications for years made me curious to learn about building websites. Taking the full-stack web development bootcamp course at UC Davis extension never fails to surprise me everyday with interesting technologies and frameworks.
-                    Looking for opportunities to work on web development projects in and around Sacramento. </p>
+                    <p className="card-text">Full stack web developer cum Automation Tester. Working on test automation of web applications for years made me curious to learn about building websites. Taking the full-stack web development bootcamp course at UC Davis extension never failed to surprise me everyday with interesting technologies and frameworks.
+                    Building web applications are real fun.</p>
                     <h4>Skillset:</h4>
                     <ul>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
+                        <li>HTML/CSS/JS</li>
+                        <li>NodeJs</li>
+                        <li>ReactJs</li>
+                        <li>Express</li>
+                        <li>SQL/MongoDB</li>
                     </ul>
                     <a href="http://www.linkedin.com/in/selvameena-s"><FaLinkedin className="aboutIcon" /></a>
                 </div>
